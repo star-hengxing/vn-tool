95d1063a346b075e377a0099802416e8b421f65b
star9029 <hengxings783@gmail.com>
2023-08-21 23:08:44 +0800

    build: improve xmake.lua

--- src/xmake/rules/module.lua
+++ src/xmake/rules/module.lua
@@ -7,21 +7,22 @@ rule("module.program")
         end
     end)
 
-    after_build(function (target)
+    after_link(function (target)
         local enabled = target:extraconf("rules", "module.program", "upx")
         if (not enabled) or (not is_mode("release")) then
             return
         end
 
         import("core.project.depend")
+        import("lib.detect.find_tool")
 
         local targetfile = target:targetfile()
         depend.on_changed(function ()
             local file = path.join("build", path.filename(targetfile))
-            local upx = assert(import("lib.detect.find_tool")("upx"), "upx not found!")
+            local upx = assert(find_tool("upx"), "upx not found!")
 
             os.tryrm(file)
-            os.execv(upx.program, {targetfile, "-o", file})
+            os.vrunv(upx.program, {targetfile, "-o", file})
         end, {files = targetfile})
     end)
 
